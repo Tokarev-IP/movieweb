447ab0f3c8c89cbad656bfb276ac9aa90468176a
TokarevIP <tokarevrab@gmail.com>
2024-04-12 23:54:32 +0100

    Show movie review excerpts; Refactor Details page.

--- src/types/interfaces.d.ts
+++ src/types/interfaces.d.ts
@@ -49,3 +49,9 @@ export interface MovieListPageTemplateProps {
     title: string;
     selectFavourite: (m: number) => void;
 }
+
+export interface Review {
+    id: string;
+    content: string
+    author: string
+}
