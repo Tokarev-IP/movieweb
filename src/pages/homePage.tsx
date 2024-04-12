ba398e982356a3b7f187498fa14af5e7dcb4dc7e
TokarevIP <tokarevrab@gmail.com>
2024-04-12 22:37:25 +0100

    Added filtering of movie list.

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,20 +1,43 @@
-import React, { useState, useEffect } from "react";
-import { ListedMovie } from "../types/interfaces"; 
+import React, { useState, useEffect } from "react";  // Changed
 import Header from "../components/headerMovieList";
+import FilterCard from "../components/filterMoviesCard";
 import Grid from "@mui/material/Grid";
 import MovieList from "../components/movieList";
-import { BaseMovieList } from "../types/interfaces";
- 
+import Fab from "@mui/material/Fab";
+import Drawer from "@mui/material/Drawer";
+import { FilterOption, ListedMovie } from "../types";
+
 const styles = {
-  root: {
-    padding: "20px",
-  },
+    root: {
+        padding: "20px",
+    }, fab: {
+        marginTop: 8,
+        position: "fixed",
+        top: 2,
+        right: 2,
+    },
 };
 
-
-
 const MovieListPage: React.FC = () => {
-    const [movies, setMovies] = useState<ListedMovie[]>([]);
+    const [movies, setMovies] = useState([]);
+    const [titleFilter, setTitleFilter] = useState("");
+    const [genreFilter, setGenreFilter] = useState("0");
+    const [drawerOpen, setDrawerOpen] = useState(false);
+
+    const genreId = Number(genreFilter);
+
+    let displayedMovies = movies
+        .filter((m: ListedMovie) => {
+            return m.title.toLowerCase().search(titleFilter.toLowerCase()) !== -1;
+        })
+        .filter((m: ListedMovie) => {
+            return genreId > 0 ? m.genre_ids.includes(genreId) : true;
+        });
+
+    const handleChange = (type: FilterOption, value: string) => {
+        if (type === "title") setTitleFilter(value);
+        else setGenreFilter(value);
+    };
 
     useEffect(() => {
         fetch(
@@ -22,6 +45,7 @@ const MovieListPage: React.FC = () => {
         )
             .then((res) => res.json())
             .then((json) => {
+                // console.log(json);
                 return json.results;
             })
             .then((movies) => {
@@ -30,15 +54,36 @@ const MovieListPage: React.FC = () => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-  return (
-    <Grid container sx={styles.root}>
-      <Grid item xs={12}>
-        <Header title={"Home Page"} />
-      </Grid>
-      <Grid item container spacing={5}>
-        <MovieList movies={movies}></MovieList>
-      </Grid>
-    </Grid>
-  );
+    return (
+        <>
+            <Grid container sx={styles.root}>
+                <Grid item xs={12}>
+                    <Header title={"Home Page"} />
+                </Grid>
+                <Grid item container spacing={5}>
+                    <MovieList movies={displayedMovies}></MovieList>
+                </Grid>
+            </Grid>
+            <Fab
+                color="secondary"
+                variant="extended"
+                onClick={() => setDrawerOpen(true)}
+                sx={styles.fab}
+            >
+                Filter
+      </Fab>
+            <Drawer
+                anchor="left"
+                open={drawerOpen}
+                onClose={() => setDrawerOpen(false)}
+            >
+                <FilterCard
+                    onUserInput={handleChange}
+                    titleFilter={titleFilter}
+                    genreFilter={genreFilter}
+                />
+            </Drawer>
+        </>
+    );
 };
 export default MovieListPage;
